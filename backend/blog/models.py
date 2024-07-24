8350cfaa7d7fccd3d9db5de5d2ec4081bf410cf9
pour-moi <pour64moi@gmail.com>
2024-07-24 13:26:08 +0300

    increased number of charField

--- backend/blog/models.py
+++ backend/blog/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Blog(models.Model):
-    title = models.CharField(max_length=50)
-    content = models.CharField(max_length=1300)
+    title = models.CharField(max_length=100)
+    content = models.CharField(max_length=2000000)
 
     def __str__(self) -> str:
         return f"{self.title}: {self.content}"
