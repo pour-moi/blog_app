a2d0c0f6810556b22cbb21633c1a2a29d8b1c8b8
pour-moi <pour64moi@gmail.com>
2024-07-25 17:30:29 +0300

    styled Login and registration page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import "./style.css";
 import axios from "axios";
 
 export default function Login() {
@@ -33,25 +34,37 @@ export default function Login() {
 
   return (
     <>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          name="username"
-          placeholder="username"
-          value={user_data.username}
-          onChange={handleUserName}
-          required
-        />
-        <input
-          type="password"
-          name="password"
-          placeholder="password"
-          value={user_data.password}
-          onChange={handlePassword}
-          required
-        />
-        <input type="submit" value="Login" />
-      </form>
+      <div className="login">
+        <div className="login-container">
+          <img
+            src="https://img.freepik.com/free-vector/account-concept-illustration_114360-399.jpg?w=740&t=st=1721916127~exp=1721916727~hmac=3855970a1edaf8e1b1500ade32389d267bb7fe214458097f9877a94fae324282"
+            alt=""
+            className="login-image"
+          />
+          <form onSubmit={handleSubmit} autoComplete="off">
+            <h1 style={{ color: "white" }}>Login</h1>
+            <input
+              type="text"
+              name="username"
+              placeholder="username"
+              value={user_data.username}
+              onChange={handleUserName}
+              className="login-username"
+              required
+            />
+            <input
+              type="password"
+              name="password"
+              placeholder="password"
+              value={user_data.password}
+              onChange={handlePassword}
+              className="login-password"
+              required
+            />
+            <input type="submit" value="Login" className="login-button" />
+          </form>
+        </div>
+      </div>
     </>
   );
 }
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,5 +1,7 @@
 import axios from "axios";
 import { useState } from "react";
+import "./style.css";
+
 export default function Register() {
   const [new_user, setNewUser] = useState({
     username: "",
@@ -30,25 +32,37 @@ export default function Register() {
 
   return (
     <>
-      <form onSubmit={handelSubmit}>
-        <input
-          type="text"
-          name="username"
-          value={new_user.username}
-          onChange={handelNewUser}
-          placeholder="username"
-          required
-        />
-        <input
-          type="password"
-          name="password"
-          value={new_user.password}
-          onChange={handelNewPassword}
-          placeholder="password"
-          required
-        />
-        <input type="submit" value="Submit" />
-      </form>
+      <div className="login">
+        <div className="login-container">
+          <img
+            src="https://img.freepik.com/free-vector/account-concept-illustration_114360-399.jpg?w=740&t=st=1721916127~exp=1721916727~hmac=3855970a1edaf8e1b1500ade32389d267bb7fe214458097f9877a94fae324282"
+            alt=""
+            className="login-image"
+          />
+          <form onSubmit={handelSubmit} autoComplete="off">
+            <h1 style={{ color: "white" }}>Register</h1>
+            <input
+              type="text"
+              name="username"
+              value={new_user.username}
+              onChange={handelNewUser}
+              placeholder="username"
+              className="login-username"
+              required
+            />
+            <input
+              type="password"
+              name="password"
+              value={new_user.password}
+              onChange={handelNewPassword}
+              placeholder="password"
+              className="login-password"
+              required
+            />
+            <input type="submit" value="Submit" className="login-button" />
+          </form>
+        </div>
+      </div>
     </>
   );
 }
