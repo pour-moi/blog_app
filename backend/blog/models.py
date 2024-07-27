12d9ccbac9accd84a98a5c0bff5630b322f5c6f6
pour-moi <pour64moi@gmail.com>
2024-07-27 12:27:55 +0300

    Author added for the model

--- backend/blog/models.py
+++ backend/blog/models.py
@@ -1,9 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
 class Blog(models.Model):
     title = models.CharField(max_length=100)
     content = models.CharField(max_length=2000000)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blogs")
 
     def __str__(self) -> str:
         return f"{self.title}: {self.content}"
